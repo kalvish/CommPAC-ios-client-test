5c959b6fd37c4adfaa0a7a79ad07ce3acb1fe505
Michael <michaelt@irononetech.com>
2016-07-18 17:51:59 +0530

    changes

--- webRTCTest/CommPACSDK/CommPACSocketIOChannel.h
+++ webRTCTest/CommPACSDK/CommPACSocketIOChannel.h
@@ -8,6 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSInteger, CommPACSocketIOChannelState) {
+    // State when disconnected.
+    kCommPACSocketIOChannelStateClosed,
+    // State when connection is established but not ready for use.
+    kCommPACSocketIOChannelStateOpen,
+    // State when connection is established and registered.
+    kCommPACSocketIOChannelStateRegistered,
+    // State when connection encounters a fatal error.
+    kCommPACSocketIOChannelStateError
+};
+
+@class CommPACSocketIOChannel;
+
+@protocol CommPACSocketIOChannelDelegate <NSObject>
+
+- (void)channel:(ARDWebSocketChannel *)channel
+ didChangeState:(ARDWebSocketChannelState)state;
+
+- (void)channel:(ARDWebSocketChannel *)channel
+didReceiveMessage:(ARDSignalingMessage *)message;
+
+@end
+
+@end
 @interface CommPACSocketIOChannel : NSObject
 
 @end
